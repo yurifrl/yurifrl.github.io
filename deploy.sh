a6f873d782252980cc726853a9be8a9351aa7dcb
yurifrl <yurifrl@hotmail.com>
2016-11-29 15:34:20 -0200

    save

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# yurifl/octopress octopress deploy
-
 docker run --rm -it -w /srv/jekyll \
   -v $HOME/.ssh:/.ssh \
   -v $(pwd):/srv/jekyll \
   -v $(pwd)/.gitconfig:/root/.gitconfig \
-  yurifl/octopress sh
+  yurifl/octopress octopress deploy
+
 
